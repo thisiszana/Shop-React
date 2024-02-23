63e9b76a61f9243012ad78f891d4b75737fdc4cd
Zana Amanallhy <zanaamanallhy80@gmail.com>
2024-02-23 18:23:34 +0330

    Create search and category sections. Creating a state query to store the user's search as well as the selected category. Data filtering based on search and classification simultaneously and separately.

--- src/Helpers/helper.js
+++ src/Helpers/helper.js
@@ -2,4 +2,22 @@ const shortenText = (text) => {
   return text.split(" ").splice(0, 3).join(" ");
 };
 
-export { shortenText };
+const searchProducts = (products, search) => {
+  if (!search) return products;
+
+  const searchedProducts = products.filter((p) =>
+    p.title.toLowerCase().includes(search)
+  );
+
+  return searchedProducts;
+};
+
+const filterProducts = (products, category) => {
+  if (!category) return products;
+
+  const filteredProducts = products.filter((p) => p.category === category);
+
+  return filteredProducts;
+};
+
+export { shortenText, searchProducts, filterProducts };
--- src/Pages/ProductsPage.jsx
+++ src/Pages/ProductsPage.jsx
@@ -1,23 +1,81 @@
+import { useEffect, useState } from "react";
+import { ImSearch } from "react-icons/im";
+import { FaListUl } from "react-icons/fa";
+
 import Card from "../Components/Card";
 import { useProducts } from "../Context/ProductsContext";
 import Loader from "../Components/Loader";
 
+import { filterProducts, searchProducts } from "../Helpers/helper.js";
 import styles from "./ProductsPage.module.css";
 
 function ProductsPage() {
   const products = useProducts();
+
+  const [search, setSearch] = useState("");
+  const [displayed, setDisplayed] = useState([]);
+  const [query, setQuery] = useState({});
+
+  useEffect(() => {
+    setDisplayed(products);
+  }, [products]);
+
+  useEffect(() => {
+    let finalProducts = searchProducts(products, query.search);
+    finalProducts = filterProducts(finalProducts, query.category);
+
+    setDisplayed(finalProducts);
+  }, [query]);
+
+  const searchHandler = () => {
+    setQuery((query) => ({ ...query, search }));
+  };
+
+  const categoryHandler = (e) => {
+    const { tagName } = e.target;
+
+    const category = e.target.innerText.toLowerCase();
+
+    if (tagName !== "LI") return;
+
+    setQuery((query) => ({ ...query, category }));
+  };
+
   return (
-    <div>
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="Search ..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase().trim())}
+        />
+        <button onClick={searchHandler}>
+          <ImSearch />
+        </button>
+      </div>
       <div className={styles.container}>
         <div className={styles.products}>
-          {!products.length && <Loader />}
-          {products.map((product) => (
+          {!displayed.length && <Loader />}
+          {displayed.map((product) => (
             <Card key={product.id} data={product} />
           ))}
         </div>
-        <div>SideBar</div>
+        <div>
+          <div>
+            <FaListUl />
+            <p>Categories</p>
+          </div>
+          <ul onClick={categoryHandler}>
+            <li>All</li>
+            <li>Electronics</li>
+            <li>Jewelery</li>
+            <li>Men's Clothing</li>
+            <li>Women's Clothing</li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
